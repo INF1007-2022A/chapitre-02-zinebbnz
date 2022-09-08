af0a77a5e9779b889dd0d23484fe6b48e67cc35c
zinebbnz <112499149+zinebbnz@users.noreply.github.com>
2022-09-08 08:36:20 -0400

    done

--- exercice.py
+++ exercice.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 def majuscule(mot):
-    # TODO completer la fonction ici
+    nouveau_mot = ""
+    for lettre in mot:
+        nouveau_mot += chr(ord(lettre) - 32)
+
+    return nouveau_mot
     return mot
 
 
